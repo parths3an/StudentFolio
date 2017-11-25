24ae10342ddb522af9fe833b683304df18fa39f4
Parth Shah <kingcobra@Parths-MacBook-Pro.local>
2017-11-24 18:58:34 -0800

    Made changes in the classes.

--- Student.h
+++ Student.h
@@ -25,6 +25,24 @@ class Student
     string id;
     double gpa;
     
-    //semesterRecord s1; 	    	
-};
+    /**
+     * A Map containing all the semester the student 
+     * has been in the college. 
+     * First para: Name of the semester 
+     * Second para: Semester Object, which contains all the info
+     *              about that specific sememster.
+     */  
+    map<string,SemesterRecord> semesters; 
+
+    /** 
+     * A map containing all the classes the student have taken
+     * First para: Name of the class(string)
+     * Second para: A pair containing units of the class and received 
+     *              grade in that class. 
+     * This map will be helpful in finding data about a specific class
+     * without knowing the semester in which the class was taken. 
+     */ 
+    
+    map<string,pair<double,string> > classesMap;
+    };
 #endif 
